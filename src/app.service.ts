6f8a2cfbfc784db665e6fd98c35c016069ee5153
gilad-ben-dor <gilad.bendor@gmail.com>
2025-09-21 15:59:24 +0300

    COMPLETE ROLLBACK

--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import type { HelloInput, HelloOutput } from './schemas/hello.schema';
-
-@Injectable()
-export class AppService {
-    getHello(input: HelloInput): HelloOutput {
-        return { a: 'Hello World! x=' + input.x, b: { c: input.y } };
-    }
-}
